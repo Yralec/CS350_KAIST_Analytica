1da2d7304915998cb990d9663c372c06b9e85442
LucLeGros <3gmichels@gmail.com>
2018-11-18 23:05:03 +0900

    added boxes needs only prediction and interval size

--- Website/Public/state.js
+++ Website/Public/state.js
@@ -85,8 +85,19 @@ State.prototype.getWinnerText = function() {
 
 }
 
+State.prototype.getHistogramPredictions = function(){
+    var list = []
+    for (var i = 0; i<this.getIntervalNumber();++i){
+        list.push(Math.random());
+    }
+    return list
+}
+State.prototype.getIntervalNumber = function() {
+    return 12
+}
 State.prototype.drawHistogram = function() {
 
+    var predictions = this.getHistogramPredictions()
     var canvas = document.getElementById("myCanvas");
     //absolute size canvas
     canvas.width  = Math.floor(window.innerWidth*0.8);
@@ -98,16 +109,57 @@ State.prototype.drawHistogram = function() {
     ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height);
     
     //histogram window
-    var startX = Math.floor(window.innerWidth*0.1)
+    var startX = Math.floor(window.innerWidth*0.15)
     var startY = Math.floor(window.innerHeight*0.1)
     var lengthX = Math.floor(window.innerWidth*0.6)
     var lengthY = Math.floor(window.innerHeight*0.6)
     ctx.rect(startX, startY, lengthX, lengthY);
     //
+    //title
+    ctx.font="20px Georgia";
+    ctx.fillText("Predicted probability that NLP wins over time in "+ this.name() + " from " + "to ",startX,startY-20);
+    //x axis title
+    ctx.font="20px Georgia";
+    ctx.fillText("Time (from a to b in months) ",startX,startY+lengthY+60);
+    //x axis numbers and boxes
+    var intervals = predictions.length
+    var maxIntervals = 12
+    var boxWidth = Math.round(lengthX/(2*maxIntervals))
+    for (var i = 1; i < intervals; i++) {
+        //x axis number
+        ctx.fillStyle = '#000000'
+        ctx.fillText(String(4*i), startX + i*Math.round(lengthX/maxIntervals), startY + lengthY + 20);
+
+        //color
+        ctx.fillStyle = getColor(predictions[i-1])
+        //boxes
+        ctx.fillRect(
+        startX + i*Math.round(lengthX/maxIntervals ) - Math.round(boxWidth/3),//x pos
+        startY + Math.round((1-predictions[i-1])*lengthY ),//y pos
+        boxWidth,//width
+        predictions[i-1]*lengthY);//height
+    }
+
+    //y axis title
+    ctx.font="20px Georgia";
+    ctx.fillStyle = '#000000'
+    ctx.fillText("Predicted",10,startY+Math.floor(lengthY/2)-2*22);
+    ctx.fillText("probability",10,startY+Math.floor(lengthY/2)-1*22);
+    ctx.fillText("that NLP",10,startY+Math.floor(lengthY/2)+0*22);
+    ctx.fillText("wins (0-1)",10,startY+Math.floor(lengthY/2)+1*22);
+    //y axis numbers
+    var precision = 9
+    ctx.fillText("1", startX - 40,startY);
+    for (var i = 1; i < precision+1; i++) {
+        ctx.fillText("0."+String(precision-i+1), startX - 40,startY+i*Math.round(lengthY/(precision+1)));
+    }
+    ctx.fillText("0", startX - 40,startY+lengthY);
+    
+    
 
     //draw color boxes
 
-    
+
     //draw 0.5 probability threshold line
     ctx.fillStyle="#FF0000";
     ctx.fillRect(startX,startY+Math.floor(lengthY/2),lengthX,Math.floor(lengthY/100));
@@ -117,4 +169,7 @@ State.prototype.drawHistogram = function() {
 
 
     ctx.stroke();//draw
+}
+function getColor(proba){
+    return 'rgb(' + String(Math.round(255*proba))+', 0, '+ Math.round(String(255*(1-proba)))+')'
 }
